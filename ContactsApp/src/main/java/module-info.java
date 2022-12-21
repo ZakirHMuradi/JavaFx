981185a80dd0c7e60302eb0525a0a12e549bbdde
ZakirHMuradi <zakir.hussainmuradi_2025@ucentralasia.org>
2022-12-21 09:47:12 +0600

    deleted

--- ContactsApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.contactsapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.contactsapp to javafx.fxml;
-    exports com.example.contactsapp;
-}
